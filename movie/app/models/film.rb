7ef8651047e248e39fa2d99219e84a9fc9c26f95
Autumn <abetterautumn@gmail.com>
2017-04-11 17:28:29 +0800

    implement film relation

--- movie/app/models/film.rb
+++ movie/app/models/film.rb
@@ -2,4 +2,6 @@ class Film < ApplicationRecord
   validates :title, presence: true
   belongs_to :user
   has_many :reviews
+  has_many :film_relationships
+  has_many :member, :through => :film_relationships, :source => :user
 end
--- movie/db/schema.rb
+++ movie/db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410042948) do
+ActiveRecord::Schema.define(version: 20170411091145) do
+
+  create_table "film_relationships", force: :cascade do |t|
+    t.integer  "film_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "film_user_relationships", force: :cascade do |t|
+    t.integer  "film_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "films", force: :cascade do |t|
     t.string   "title"
